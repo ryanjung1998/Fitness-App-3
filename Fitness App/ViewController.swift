d8b25a5be197e3890f6d23cdd87aa550446046d9
Ryan Jung <ryanjung1998@gmail.com>
2022-11-27 17:19:06 -0700

    Initial Commit

---
+++ Fitness App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fitness App
+//
+//  Created by Ryan Jung on 2022-11-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
