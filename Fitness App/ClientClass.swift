6879d1f539986568c2b1ed73b1405d4b31540d10
Fardin Mahid <99063793+Freddydeddydoo@users.noreply.github.com>
2022-12-02 15:55:49 -0700

    User

---
+++ Fitness App/ClientClass.swift
@@ -0,0 +1,13 @@
+//
+//  Client.swift
+//  Fitness App
+//
+//  Created by Fardin Mahid on 2022-12-02.
+//
+
+import Foundation
+import UserClass
+
+class Client:User{
+    
+}
--- Fitness App/UserClass.swift
+++ Fitness App/UserClass.swift
@@ -10,18 +10,25 @@ import SQLite3
 
 
 class User{
-    final var user :Int
+    final var userID :Int
     var ethnicity : String
     var countryOfResidence : String
     var gender : String
     var birthDay : Date
+    static var counter : Int = 0
     
-    init(user: Int, ethnicity: String, countryOfResidence: String, gender: String, birthDay: Date) {
-        self.user = user
+    init(ethnicity: String, countryOfResidence: String, gender: String, birthDay: Date) {
+        //self.user = user
+        /*
+         userID will be a value not in the database
+         so we would have to seach the database
+         */
         self.ethnicity = ethnicity
         self.countryOfResidence = countryOfResidence
         self.gender = gender
         self.birthDay = birthDay
+        self.userID = User.counter
+        User.counter += 1 // for now
     }
     
     public func instert(usr:User) -> Bool{
