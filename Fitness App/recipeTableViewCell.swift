5de12e702188baf10fb6d50d0654081dc40ba021
ryanjung1998 <119268423+ryanjung1998@users.noreply.github.com>
2022-12-05 20:27:12 -0700

    Create recipeTableViewCell.swift

---
+++ Fitness App/recipeTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  recipeTableViewCell.swift
+//  Fitness App
+//
+//  Created by Ryan Jung on 2022-12-04.
+//
+
+import Foundation
+import UIKit
+
+class RecipeTableViewCell : UITableViewCell
+{
+    
+    @IBOutlet weak var rName: UILabel!
+    
+    @IBOutlet weak var creatorID: UILabel!
+}
